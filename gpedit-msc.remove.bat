9cf93f57bebd144478f5dc7ec832e3127be2bf59
GDaily <admin@gdaily.org>
2020-09-10 23:30:45 +0800

    Delete gpedit-msc.remove.bat

--- gpedit-msc.remove.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd "%~dp0"
-dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt
-dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt
-for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /remove-package:"%SystemRoot%\servicing\Packages\%%i"
-pause
